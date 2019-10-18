3dc9767475ae0288c120098ca15675731e585625
jamestehaley <52868809+jamestehaley@users.noreply.github.com>
2019-10-18 14:15:35 +0100

    completed Error page

--- nc-news/src/components/Errors.jsx
+++ nc-news/src/components/Errors.jsx
@@ -5,10 +5,10 @@ export default function Errors() {
   if (window.history.state) {
     msg = window.history.state.msg;
   } else if (window.location.pathname === '/Error') {
-    msg =
-      'An error has occurred, please press the back button to return to using NC News';
+    msg = 'An error has occurred.';
   } else {
     msg = `Page not found! "${window.location.pathname}" is not a valid path.`;
   }
-  return <div>{msg}</div>;
+  msg += ' Please press the back button to return to using NC News';
+  return <h1 className="Error">{msg}</h1>;
 }
